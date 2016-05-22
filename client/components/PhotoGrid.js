8fc25594827338a7cc2c1cf77a3a5f5a989328e3
Federico Knüssel <fknussel@gmail.com>
2016-05-23 00:19:50 +1000

    Add photo grid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+import Photo from './Photo';
+
 export default class PhotoGrid extends React.Component {
   render() {
     return (
       <div className="photo-grid">
-        I'm a photo grid
+        {this.props.posts.map((post, index) => <Photo key={index} post={post} index={index} {...this.props} />)}
       </div>
     );
   }
---
+++ client/reducers/posts.js
@@ -0,0 +1,14 @@
+export default function posts(state = [], action) {
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const index = action.index;
+
+      return [
+        ...state.slice(0,index), // before the one we are updating
+        {...state[index], likes: state[index].likes + 1},
+        ...state.slice(index + 1), // after the one we are updating
+      ]
+    default:
+      return state;
+  }
+}
